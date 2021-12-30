102941ff4381cf2ff63953ab2be53332ddfda4f8
vuoso-l <vuosolucas@gmail.com>
2021-12-30 16:16:48 -0300

    Add components to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import Bootstrap from './components/Bootstrap';
 import Bulma from './components/Bulma';
-import MaterialUi from './components/MaterialUi';
 import ReactBootstrap from './components/ReactBootstrap';
 
 function App() {
   return (
     <div>
       <h1>Frameworks CSS con React</h1>
-      {/* <Bootstrap></Bootstrap> */}
-      {/* <Bulma></Bulma> */}
-      {/* <ReactBootstrap></ReactBootstrap> */}
-      <MaterialUi></MaterialUi>
+      <Bootstrap></Bootstrap>
+      <Bulma></Bulma>
+      <ReactBootstrap></ReactBootstrap>
     </div>
   );
 }
--- src/components/MaterialUi.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function MaterialUi() {
-    return (
-        <>
-            <h2>Material UI</h2>
-        </>
-    )
-}
